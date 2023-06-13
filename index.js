bb7088c7f720a7d73bd9fada10938931d8953748
Breadamonium <yp89@cornell.edu>
2023-06-12 19:59:33 -0700

    inquirer committed, one question done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+var inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+        type: "input",
+        name: "NameQuestion",
+        message: "What is your name"
+    }
+  ])
+  .then((answers) => {
+    console.log(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("there is an error");
+    } else {
+      console.log("there is an error");
+    }
+  });
