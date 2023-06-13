57c48eb34a14652ba17abdca7419e04c1dd76d01
Breadamonium <yp89@cornell.edu>
2023-06-12 20:07:42 -0700

    fs module done, can write to a file

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 var inquirer = require('inquirer');
+var fs = require('fs');
 
 inquirer
   .prompt([
     {
         type: "input",
-        name: "NameQuestion",
-        message: "What is your name"
+        name: "nameQuestion",
+        message: "What is your name?"
+    },
+    {
+        type: "input",
+        name: "githubQuestion",
+        message: "What is your github handle?"
+    },
+    {
+        type: "input",
+        name: "locationQuestion",
+        message: "Where do you live?"
+    },
+    {
+        type: "input",
+        name: "linkedInQuestion",
+        message: "What is your linkedin url?"
+    },
+    {
+        type: "input",
+        name: "bioQuestion",
+        message: "What is your favorite food?"
     }
   ])
   .then((answers) => {
     console.log(answers);
+    fs.appendFile("index.html", answers.nameQuestion, (error) => error ? console.log(error) : console.log("Success"));
   })
   .catch((error) => {
     if (error.isTtyError) {
